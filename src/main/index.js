b61ba8ea3dd6560c4fe3762ca4d4302700deed00
WaldnerThomas <twa2384@students.fhv.at>
2023-12-13 12:40:29 +0100

    AAAAAHHH

--- src/main/index.js
+++ src/main/index.js
@@ -6,3 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
         texts.forEach(text => text.classList.toggle("active"));
     });
 });
+
+
+// moveable Box
+function anime(){
+    $('#box1')
+        .animate({'marginLeft':'20px'},1000)
+        .animate({'marginLeft':'-20px'},1000);
+}
+
+function anime2(){
+    $('#box2')
+        .animate({'opacity':'1'},500)
+        .animate({'opacity':'0.7'},500);
+    setTimeout("anime2()", 1000);
+}
+
+// TODO: Delete later, just for testing
+function test() {
+    $('#box1')
+        .animate({'marginLeft':'20px'},1000)
+        .animate({'marginLeft':'-20px'},1000);}
