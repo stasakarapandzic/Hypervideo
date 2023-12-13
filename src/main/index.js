bb338877f8052453eb8deba1f7b4f855ae9df6f1
WaldnerThomas <twa2384@students.fhv.at>
2023-12-13 12:56:23 +0100

    made a moveable button, yay

--- src/main/index.js
+++ src/main/index.js
@@ -14,16 +14,3 @@ function anime(){
         .animate({'marginLeft':'20px'},1000)
         .animate({'marginLeft':'-20px'},1000);
 }
-
-function anime2(){
-    $('#box2')
-        .animate({'opacity':'1'},500)
-        .animate({'opacity':'0.7'},500);
-    setTimeout("anime2()", 1000);
-}
-
-// TODO: Delete later, just for testing
-function test() {
-    $('#box1')
-        .animate({'marginLeft':'20px'},1000)
-        .animate({'marginLeft':'-20px'},1000);}
